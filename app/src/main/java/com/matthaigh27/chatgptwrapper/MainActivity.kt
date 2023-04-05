6238bd7ec1142bfa05425c352e688e0d1c4cd68d
Alex Newman <posix4e@gmail.com>
2023-04-05 15:58:45 -0400

    Switch to hf
    	Ship debug android build
    	Allow microphone interaction

--- app/src/main/java/com/matthaigh27/chatgptwrapper/MainActivity.kt
+++ app/src/main/java/com/matthaigh27/chatgptwrapper/MainActivity.kt
@@ -1,31 +1,34 @@
 package com.matthaigh27.chatgptwrapper
 
+import android.Manifest
 import android.annotation.SuppressLint
+import android.annotation.TargetApi
 import android.app.Activity
-import android.content.Intent
+import android.content.Context
+import android.content.pm.PackageManager
 import android.graphics.Color
 import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.view.WindowManager
+import android.webkit.PermissionRequest
+import android.webkit.WebChromeClient
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import android.window.OnBackInvokedDispatcher
+import androidx.core.app.ActivityCompat
 import com.matthaigh27.chatgptwrapper.databinding.ActivityMainBinding
-
 
 class MainActivity : Activity() {
     private val userAgent =
         "Mozilla/5.0 (Linux; Android 10) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.5563.57 Mobile Safari/537.36"
-    private val chatUrl = "https://chat.openai.com/"
+    private val chatUrl = "https://posix4e-langchainback2.hf.space/"
     private lateinit var binding: ActivityMainBinding
     private lateinit var webView: WebView
 
     @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         webView = binding.webView
@@ -49,14 +52,16 @@ class MainActivity : Activity() {
         webView.settings.domStorageEnabled = true
         webView.settings.javaScriptEnabled = true
         webView.webViewClient = WebViewClient()
-
+        webView.webChromeClient = object : WebChromeClient() {
+            @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+            override fun onPermissionRequest(request: PermissionRequest) {
+                request.grant(request.resources)
+            }
+        }
         webView.loadUrl(chatUrl)
-//        val intent = Intent(Intent.ACTION_MAIN)
-  //      intent.addCategory(Intent.CATEGORY_HOME)
-  //      intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
- //       startActivity(intent)
-//        finish()
+
     }
+
 
     @Deprecated("Deprecated in Java")
     override fun onBackPressed() {
@@ -66,4 +71,5 @@ class MainActivity : Activity() {
         else
             super.onBackPressed()
     }
+
 }
